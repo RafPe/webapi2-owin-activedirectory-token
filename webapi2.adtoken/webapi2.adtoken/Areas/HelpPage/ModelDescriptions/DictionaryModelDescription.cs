6ddb4ddf4fbc25215c631af7cacef85d789cde47
RafPe <rafal@pieniazek.nl>
2015-12-27 00:35:29 +0100

    Initial project created

---
+++ webapi2.adtoken/webapi2.adtoken/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace webapi2.adtoken.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ webapi2.adtoken/webapi2.adtoken/Areas/HelpPage/ModelDescriptions/IModelDocumentationProvider.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Reflection;
+
+namespace webapi2.adtoken.Areas.HelpPage.ModelDescriptions
+{
+    public interface IModelDocumentationProvider
+    {
+        string GetDocumentation(MemberInfo member);
+
+        string GetDocumentation(Type type);
+    }
+}
